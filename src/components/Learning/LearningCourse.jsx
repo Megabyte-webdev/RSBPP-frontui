0c7dc2e471545bf258317776cfdef70d39854f73
George-Gyang <georgegyang3@gmail.com>
2024-04-29 01:34:03 +0100

    modify

--- src/components/Learning/LearningCourse.jsx
+++ src/components/Learning/LearningCourse.jsx
@@ -55,7 +55,7 @@ const hasItem = cartList?.some(item => item.id === course.id)
       });
   };
   return (
-    <div className='rounded bg-white mb-3'>
+    <div className='rounded bg-white mb-3 h-100 d-flex flex-column justify-content-between'>
       <div onClick={() => navigate(`/learning/${course.title}`, { state: { course: course } })} className='nav-link pointer'>
         <div>
           <img src={featurePics} alt="" className="img-fluid w-100" />
