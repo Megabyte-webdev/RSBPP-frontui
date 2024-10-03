ef0857365ce2058f7c2d5bb95d9353f82d673d54
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-03 23:44:40 +0100

    Update Programmes.jsx

--- src/components/onlineprograms/Programmes.jsx
+++ src/components/onlineprograms/Programmes.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Programmes = () => {
   return (
-    <div className="h-max flex flex-col gap-y-4 sm:flex-row md:items-stretch px-2 bg-gray-400">
+    <div className="h-max flex flex-col gap-y-4 sm:flex-row md:items-stretch px-2 bg-gray-100">
         {/* Left side */}
-        <div className="relative w-full min-h-[500px] sm:min-h-full md:w-1/4 text-white py-2">
+        <div className="relative top-[-10px] w-full min-h-[500px] sm:min-h-full md:w-1/4 text-white py-2">
         <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full z-0' src={img} alt="" />
         <div className='z-1 bg-[#8B0002] opacity-[0.61] absolute top-0 right-0 bottom-0 left-0 m-auto' ></div> 
             <div className='z-2 w-full h-full p-4 absolute top-0 right-0 bottom-0 left-0 m-auto'>
@@ -21,7 +21,7 @@ const Programmes = () => {
             </div>
         </div>
         {/* Right side */}
-        <div className="bg-gray-400 px-4 mt-5 md:px-10 flex-1 w-full min-h-[600px] md:w-3/4 text-[#8B0002]">
+        <div className="bg-gray-100 px-4 mt-5 md:px-10 flex-1 w-full min-h-[600px] md:w-3/4 text-[#8B0002]">
             <section className='py-2'>
             <h2 className='font-semibold text-3xl my-3'>Online Management & Communication Programmes</h2>
             <ul className='flex flex-wrap gap-y-3 justify-between px-0 font-bold'>
