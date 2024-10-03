01b1faf59bacc0685409d05b8f235419bdca27c0
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-03 18:09:31 +0100

    Update Navbar.jsx

--- src/components/onlineprograms/Navbar.jsx
+++ src/components/onlineprograms/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo.svg";
 const Navbar = () => {
   return (
     <div>
-        <div className='text-white gap-2 py-[10px] px-3 bg-[#8B0002] px flex justify-center items-center md:justify-between'>
+        <div className='text-xs md:text-sm text-white gap-2 py-[10px] px-3 bg-[#8B0002] px flex justify-center items-center md:justify-between'>
             <div className='flex gap-2'>
             <a className='no-underline text-white font-bold' href="tel:+31(0)10 307 2137">+31(0)10 307 2137</a>
             <a className='no-underline text-white font-bold' href="mailto:info@rsbpp.nl">info@rsbpp.nl</a>
@@ -12,7 +12,7 @@ const Navbar = () => {
 
             </div>
         </div>
-        <div className="px-3 flex justify-between items-center text-sm py-3">
+        <div className="px-3 flex justify-between items-center text-sm py-4">
       <img className="w-32 md:w-60 cursor-pointer" src={logo} alt="logo" />
       <ul className="px-5 flex-1 text-[15px] text-black font-normal hidden md:flex md:flex-wrap items-center justify-center gap-x-5 gap-y-2">
         <NavLink className="active:text-[#8B0002] no-underline text-inherit" to="/">
