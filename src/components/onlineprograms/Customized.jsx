2f9e66df964e5674dc34f2ad695eb02d8f369819
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-03 22:56:40 +0100

    Update Customized.jsx

--- src/components/onlineprograms/Customized.jsx
+++ src/components/onlineprograms/Customized.jsx
@@ -9,7 +9,7 @@ const Customized = () => {
                 <h3 className='flex-1 text-center text-2xl md:text-4xl font-medium mx-3 md:p-3'>Customised Online Programmes
                 </h3>
             </div>
-            <p className='text-sm md:text-xl font-medium my-4'>Is your topic or subject of interest not captured on the web? Never mind! We can design and tailor solutions that can be delivered wherever and whenever for individuals  wishing to learn or improve on their skills or organizations willing to upskill their employees. We will work closely with you to tailor contents/curriculum design.</p>
+            <p className='text-sm md:text-xl font-medium my-7'>Is your topic or subject of interest not captured on the web? Never mind! We can design and tailor solutions that can be delivered wherever and whenever for individuals  wishing to learn or improve on their skills or organizations willing to upskill their employees. We will work closely with you to tailor contents/curriculum design.</p>
         </div>
         {/* Right side */}
         <div className='w-full md:min-h-[400px] lg:ml-3 md:w-[40%]'>
