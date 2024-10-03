78d3534a2a45d3921d54d5b7958d5129175a54d2
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-03 18:46:05 +0100

    Update Customized.jsx

--- src/components/onlineprograms/Customized.jsx
+++ src/components/onlineprograms/Customized.jsx
@@ -3,9 +3,9 @@ const Customized = () => {
   return (
     <div className='flex flex-col md:flex-row px-4 justify-between my-10'>
         {/* Left side */}
-        <div className='flex-1 w-full md:w-1/2'>
+        <div className='flex-1 w-full md:w-1/2 md:pr-4'>
             <div className='flex flex-wrap md:flex-nowrap my-2 gap-3 justify-between'>
-                <p className='text-xl md:text-5xl px-3 text-black bg-white border border-gray-900'>C</p>
+                <p className='text-xl md:text-5xl px-2 text-black bg-white border border-gray-900'>C</p>
                 <h3 className='text-2xl md:text-4xl font-medium mx-3 md:p-3'>Customised Online Programmes
                 </h3>
             </div>
