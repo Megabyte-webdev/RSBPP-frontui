eb4323cb926697d9634a18406afbfc839dde36f2
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 08:34:05 +0100

    Update Banner.jsx

--- src/components/onlineprograms/Banner.jsx
+++ src/components/onlineprograms/Banner.jsx
@@ -6,17 +6,17 @@ const Banner =()=>{
     const navigate = useNavigate()
 
     return (
-        <div className="relative flex bg-primary rounded-lg px-4 md:px-10 h-max">
-      <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full min-h-[250px] md:min-h-[300px] z-0' src={img} alt="" />
+        <div className="relative flex items-center bg-primary rounded-lg px-4 md:px-10 min-h-[400px]">
+      <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full z-0' src={img} alt="" />
        <div className='z-1 bg-[#8B0002] opacity-[0.61] absolute top-0 right-0 bottom-0 left-0 m-auto' ></div> 
             {/* --------Left Side------ */}
-            <div className="flex-1 p-5 sm:p-10  z-2 absolute top-0 right-0 bottom-0 left-0 m-auto h-full">
+            <div className="flex-1 p-5 sm:p-10  z-2 absolute top-0 right-0 bottom-0 left-0 m-auto h-max">
                 <div className="text-2xl md:text-3xl lg:text-5xl font-semibold text-white m-4">
                     <p className="">Nothing Beats the right skills for personal and organisational growth</p>
                 </div>
 
 <p className='text-sm md:text-xl'> No matter the expertise level you intend to pursue, we always have the right programme for you</p>
-                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-white px-8 py-3 mt-6 hover:scale-105 transition-all">Register now</button>
+                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-white px-8 py-2 mt-3 hover:scale-105 transition-all">Register now</button>
             </div>
             {/* --------Right Side------ */}
             <div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
