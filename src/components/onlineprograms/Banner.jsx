1fefb1c22024752316da0f95d77d0f9e4730856a
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 08:23:31 +0100

    Update Banner.jsx

--- src/components/onlineprograms/Banner.jsx
+++ src/components/onlineprograms/Banner.jsx
@@ -10,12 +10,13 @@ const Banner =()=>{
       <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full z-0' src={img} alt="" />
        <div className='z-1 bg-[#8B0002] opacity-[0.61] absolute top-0 right-0 bottom-0 left-0 m-auto' ></div> 
             {/* --------Left Side------ */}
-            <div className="flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5 z-2 absolute top-0 right-0 bottom-0 left-0 m-auto">
+            <div className="flex-1 p-5 sm:p-10  z-2 absolute top-0 right-0 bottom-0 left-0 m-auto">
                 <div className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white">
                     <p className="mt-4">Nothing Beats the right skills for personal and organisational growth</p>
-<p> No matter the expertise level you intend to pursue, we always have the right programme for you</p>
                 </div>
-                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-gray-600 px-8 py-3 mt-6 hover:scale-105 transition-all">Register now</button>
+
+<p className='text-sm md:text-xl'> No matter the expertise level you intend to pursue, we always have the right programme for you</p>
+                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-white px-8 py-3 mt-6 hover:scale-105 transition-all">Register now</button>
             </div>
             {/* --------Right Side------ */}
             <div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
