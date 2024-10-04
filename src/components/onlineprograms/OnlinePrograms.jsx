1f0346286233426a093943b93f51f5fe58677a25
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 07:50:15 +0100

    Update OnlinePrograms.jsx

--- src/components/onlineprograms/OnlinePrograms.jsx
+++ src/components/onlineprograms/OnlinePrograms.jsx
@@ -1,12 +1,15 @@
 import Hero from './Hero'
 import Customized from './Customized'
 import Programmes from './Programmes'
+import Banner from './Banner'
+
 const OnlinePrograms = () => {
   return (
     <>
       <Hero />
       <Customized />
       <Programmes />
+      <Banner />
     </>
   )
 }
