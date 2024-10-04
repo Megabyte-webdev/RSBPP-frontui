67a85b2f298b7c8569f03c256ae2e9e156a84cc5
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 08:32:02 +0100

    Update Banner.jsx

--- src/components/onlineprograms/Banner.jsx
+++ src/components/onlineprograms/Banner.jsx
@@ -7,12 +7,12 @@ const Banner =()=>{
 
     return (
         <div className="relative flex bg-primary rounded-lg px-4 md:px-10 h-max">
-      <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full z-0' src={img} alt="" />
+      <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full min-h-[250px] md:min-h-[300px] z-0' src={img} alt="" />
        <div className='z-1 bg-[#8B0002] opacity-[0.61] absolute top-0 right-0 bottom-0 left-0 m-auto' ></div> 
             {/* --------Left Side------ */}
-            <div className="flex-1 p-5 sm:p-10  z-2 absolute top-0 right-0 bottom-0 left-0 m-auto min-h-300px">
-                <div className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white">
-                    <p className="mt-4">Nothing Beats the right skills for personal and organisational growth</p>
+            <div className="flex-1 p-5 sm:p-10  z-2 absolute top-0 right-0 bottom-0 left-0 m-auto h-full">
+                <div className="text-2xl md:text-3xl lg:text-5xl font-semibold text-white m-4">
+                    <p className="">Nothing Beats the right skills for personal and organisational growth</p>
                 </div>
 
 <p className='text-sm md:text-xl'> No matter the expertise level you intend to pursue, we always have the right programme for you</p>
