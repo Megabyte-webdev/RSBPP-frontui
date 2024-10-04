4ca375d69e5656e278e8269561726abb7c594e6f
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 16:52:47 +0100

    Update Partners.jsx

--- src/components/onlineprograms/Partners.jsx
+++ src/components/onlineprograms/Partners.jsx
@@ -7,8 +7,8 @@ import partner4 from '../../assets/online-programmes/Docenti-removebg-preview.pn
 const Partners = () => {
   return (
     <div className='px-4 flex flex-col md:flex-row my-3'>
-        <h3 className='text-2xl md:text-3xl text-black border-b border-b-gray-500 py-3'>Our Partners</h3>
-        <div className='md:flex-1 w-full overflow-scroll flex gap-3 pt-3'>
+        <h3 className='h-max text-2xl md:text-3xl text-black border-b border-b-gray-500 py-3'>Our Partners</h3>
+        <div className='md:flex-1 w-full overflow-x-scroll scrollbar-hide flex gap-3 pt-3 md:mx-4'>
             <img className='w-24' src={partner1} alt="" />
             <img className='w-24 object-contain' src={partner2} alt="" />
             <img className='w-24 object-contain' src={partner3} alt="" />
