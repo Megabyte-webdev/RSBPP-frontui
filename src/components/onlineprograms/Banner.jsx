047d38fa5856de4d58c045a1e78265d2a9b0c912
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 08:16:37 +0100

    Update Banner.jsx

--- src/components/onlineprograms/Banner.jsx
+++ src/components/onlineprograms/Banner.jsx
@@ -15,7 +15,7 @@ const Banner =()=>{
                     <p className="mt-4">Nothing Beats the right skills for personal and organisational growth</p>
 <p> No matter the expertise level you intend to pursue, we always have the right programme for you</p>
                 </div>
-                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all">Register now</button>
+                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-gray-600 px-8 py-3 mt-6 hover:scale-105 transition-all">Register now</button>
             </div>
             {/* --------Right Side------ */}
             <div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
