3e344f3c5b5c6c085d2c86316aff182d3bf180e2
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 08:41:02 +0100

    Update Banner.jsx

--- src/components/onlineprograms/Banner.jsx
+++ src/components/onlineprograms/Banner.jsx
@@ -10,13 +10,13 @@ const Banner =()=>{
       <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full z-0' src={img} alt="" />
        <div className='z-1 bg-[#8B0002] opacity-[0.61] absolute top-0 right-0 bottom-0 left-0 m-auto' ></div> 
             {/* --------Left Side------ */}
-            <div className="flex-1 p-5 sm:p-10  z-2 absolute top-0 right-0 bottom-0 left-0 m-auto h-max text-white">
+            <div className="flex-1 p-3 sm:p-10  z-2 absolute top-0 right-0 bottom-0 left-0 m-auto h-max text-white">
                 <div className="text-2xl md:text-3xl lg:text-5xl font-semibold text-white my-4">
                     <p>Nothing Beats the right skills for personal and organisational growth</p>
                 </div>
 
 <p className='text-sm md:text-xl'> No matter the expertise level you intend to pursue, we always have the right programme for you</p>
-                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-white px-8 py-2 my-3 hover:scale-105 underline transition-all">Explore More</button>
+                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-[#8B0002] text-sm sm:text-base text-white px-8 py-2 my-3 hover:scale-105 underline font-medium transition-all">Explore More</button>
             </div>
             {/* --------Right Side------ */}
             <div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
