725196a301b0784f8b344e82bd15e71625739118
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-05 10:40:38 -1100

    Update ExploreDigiKnowH.jsx

--- src/components/onlineprograms/ExploreDigiKnowH.jsx
+++ src/components/onlineprograms/ExploreDigiKnowH.jsx
@@ -30,7 +30,7 @@ Overall, the DigiKnowH programme promises an engaging and enriching experience f
 </div>
 {/* Right side*/}
 <div className='w-full md:w-[350px]' >
-<img className='h-[300px] md:h-[600px] object-cover' src={DigiImg} alt=""/>
+<img className='w-full h-[300px] md:h-[600px] object-cover' src={DigiImg} alt=""/>
 <h3 className='my-3 text-2xl font-medium'>Digital Skills Programme ( DigiknowH)</h3>
 <ul className='flex flex-wrap gap-y-3 justify-between px-0 font-medium'>
               <Link className='flex-initial basis-full text-sm md:text-[17px] underline text-inherit flex items-center'><p><FaCheck className='text-xl mr-2 text-red-700' /></p> Effective Social Media Strategy</Link>
