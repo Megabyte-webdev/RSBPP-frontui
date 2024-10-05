66661caa751fe682c813130d5c2993eed55bd39a
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 15:06:39 -1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,7 @@ import VideoCallClass from './components/video-audio-demo/src/VideoCallClass'
 import LoginOtpForm from './components/auth/LoginOtpForm'
 import OnlineProgramsLayout from './components/onlineprograms/OnlineProgramsLayout'
 import OnlinePrograms from './components/onlineprograms/OnlinePrograms'
+import Programs from './components/onlineprograms/Programs'
 // const LazyMeeting = lazy(() => ./components/onlineprograms/OnlineProgramsLayout
 
 const App = () => {
@@ -68,6 +69,7 @@ const App = () => {
           <Routes>
           <Route path='/online-programmes' element={<OnlineProgramsLayout />} >
           <Route index element={<OnlinePrograms />} />
+<Route path='/:id' element={<Program />} />
           </Route>
             <Route path="/" element={<Layout />}>
               <Route index element={role === "admin" ? <AdminDashboard /> : role === "instructor" ? <FacultyDashboard /> : <DashboardTwo />} />
