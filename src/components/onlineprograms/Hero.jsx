1cf0968bba0b2928c3171ca0faa0c7ac86019344
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-04 13:54:15 -1100

    Update Hero.jsx

--- src/components/onlineprograms/Hero.jsx
+++ src/components/onlineprograms/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({programme =true}) => {
       <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full z-0' src={img} alt="" />
        <div className='z-1 bg-[#8B0002] opacity-[0.61] absolute top-0 right-0 bottom-0 left-0 m-auto' ></div> 
      {
-programme ?       <div className='relative z-10 h-full text-white flex flex-col justify-center px-[5%] gap-y-2'> 
+programme ?       <div className='relative z-10 h-full text-white flex flex-col justify-center p-[5%] gap-y-2'> 
 <p className='bg-[#8B0002] w-max px-2 h-max'>Online Programmes</p>
 
         <h1 className='font-medium text-3xl md:text-5xl my-2'>Programmes, understanding the principle of marketing</h1>
