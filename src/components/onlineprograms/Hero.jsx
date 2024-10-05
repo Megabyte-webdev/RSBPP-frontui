86b0a76ad94d55df326fa1b73e69bfc14bcb8293
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-05 02:47:05 -1100

    Update Hero.jsx

--- src/components/onlineprograms/Hero.jsx
+++ src/components/onlineprograms/Hero.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 import img from '../../assets/online-programmes/online-programmes-img.jpg'
 const Hero = ({programme =false, title }) => {
     return (
-    <div className='relative min-h-[200px] md:min-h-[350px]'>
+    <div className='relative min-h-[200px] md:min-h-[350px] flex justify-center align-center'>
       <img className='object-cover absolute top-0 right-0 bottom-0 left-0 m-auto w-full h-full z-0' src={img} alt="" />
        <div className='z-1 bg-[#8B0002] opacity-[0.61] absolute top-0 right-0 bottom-0 left-0 m-auto' ></div> 
      {
@@ -14,7 +14,7 @@ programme ?       <div className='relative z-10 min-h-[400px] text-white flex fl
 <p>Start Duration : 10 Hours </p>
 
         </div>
-:<div className='bg-black relative z-10 h-full text-white flex flex-col justify-center p-[5%]'> 
+:<div className='bg-black relative z-10 min-h-full text-white flex flex-col justify-center p-[5%]'> 
         <h1 className='font-medium text-3xl md:text-5xl my-3'>Online Programmes</h1>
         <Link to='https://rsbpp.nl/' className='no-underline text-inherit py-1 px-2 w-max border border-gray-300 font-bold'>Home <span className='px-2'>&gt;</span> </Link>
         </div>
