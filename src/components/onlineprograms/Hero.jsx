24c41e8a5ad81b15fb98b1b638df2a0586ac6d19
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-05 06:24:30 -1100

    Update Hero.jsx

--- src/components/onlineprograms/Hero.jsx
+++ src/components/onlineprograms/Hero.jsx
@@ -14,7 +14,7 @@ programme ?       <div className='relative z-10 min-h-[400px] text-white flex fl
 <p>Start Duration : 10 Hours </p>
 
         </div>
-:<div className='relative z-10 w-full min-h-full text-white flex flex-col justify-end p-[5%]'> 
+:<div className='relative z-10 w-full min-h-full text-white flex flex-col justify-end p-[5%] mb-2'> 
         <h1 className='font-medium text-3xl md:text-5xl my-3'>{title}</h1>
         <Link to='https://rsbpp.nl/' className='no-underline text-inherit px-2 w-max border border-gray-300 font-bold'>Home <span className='px-2'>&gt;</span> </Link>
         </div>
