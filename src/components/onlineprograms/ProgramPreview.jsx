ba35ca7b6d321aa08637b0b9b503858341538252
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-06 03:55:23 -1100

    Update ProgramPreview.jsx

--- src/components/onlineprograms/ProgramPreview.jsx
+++ src/components/onlineprograms/ProgramPreview.jsx
@@ -10,16 +10,16 @@ return(
 <p className='text-xs md:text-sm text-gray-700 my-3'>This course explores the dynamic landscape of digital marketing and its pivotal role in modern business strategies.</p>
 <p className='text-xs md:text-sm text-gray-700 my-3'>In today’s digital world, it is imperative for businesses to connect with a global audience, target specific demographics, measure campaign performance with precision, and deliver personalized marketing messages.</p>
 
-<p className='text-sm md:text-xl font-medium text-blackpy-3'> Target Participants
+<p className='text-sm font-medium text-blackpy-3'> Target Participants
 </p>
-<p className='text-sm md:text-xl text-red-600 font-medium'>For those in Digital Marketing
+<p className='text-sm text-red-600 font-medium'>For those in Digital Marketing
 </p>
 </div>
 {/* Right side*/}
 <div className='w-full md:w-1/3 flex flex-col items-center' >
-<p className='w-full md:w-[90%] text-xl font-medium text-black border border-gray-700 px-5 py-3'> Add To Cart
+<p className='w-full md:w-[90%] text-sm font-medium text-black border border-gray-700 px-5 py-3'> Add To Cart
 </p>
-<p className='w-full md:w-[90%] text-xl font-medium text-black border border-gray-700 px-5 py-3'>$300
+<p className='w-full md:w-[90%] text-sm font-medium text-black border border-gray-700 px-5 py-3'>$300
 </p>
 </div>
 
