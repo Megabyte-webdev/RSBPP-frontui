6c391d610b5215a897882f3a1fc71f6290cc834c
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-06 03:53:04 -1100

    Update ProgramPreview.jsx

--- src/components/onlineprograms/ProgramPreview.jsx
+++ src/components/onlineprograms/ProgramPreview.jsx
@@ -7,9 +7,8 @@ return(
 {/* Left side*/}
 <div className='flex-1 w-full md:w-3/4'>
 
-<p className='text-xs md:text-sm text-gray-700 my-4' >This programme is tailored for people who are curious and open to learning about digital technologies, tools, and practices.</p>
-<p className='text-xs md:text-sm text-gray-700 my-2'>This course explores the dynamic landscape of digital marketing and its pivotal role in modern business strategies.</p>
-<p className='text-xs md:text-sm text-gray-700 my-4'>In today’s digital world, it is imperative for businesses to connect with a global audience, target specific demographics, measure campaign performance with precision, and deliver personalized marketing messages.</p>
+<p className='text-xs md:text-sm text-gray-700 my-3'>This course explores the dynamic landscape of digital marketing and its pivotal role in modern business strategies.</p>
+<p className='text-xs md:text-sm text-gray-700 my-3'>In today’s digital world, it is imperative for businesses to connect with a global audience, target specific demographics, measure campaign performance with precision, and deliver personalized marketing messages.</p>
 
 <p className='text-sm md:text-xl font-medium text-blackpy-3'> Target Participants
 </p>
