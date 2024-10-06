2d5d95bc1a63a4ad5b67097d5c37d59f437e43b2
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-06 00:46:11 -1100

    Update Programme.jsx

--- src/components/onlineprograms/Programme.jsx
+++ src/components/onlineprograms/Programme.jsx
@@ -1,9 +1,11 @@
 import Hero from './Hero'
+import ProgramPreview from './ProgramPreview'
 import {useParams} from 'react-router-dom' 
 const Programme = ()=>{
     const {program} = useParams();
     return (
 <Hero programme={true} title={program} />
+<ProgramPreview />
 
 )
 
