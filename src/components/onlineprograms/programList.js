a0ffafee6e2b3f83bc90d76401c78b08046a7c2f
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-06 23:08:06 -1100

    Update programList.js

---
+++ src/components/onlineprograms/programList.js
@@ -0,0 +1,49 @@
+const programList =[
+{
+title: "Understanding Digital Customer Effectiveness Landscape",
+},{
+title: "Big Data and Business Decisions",
+},{
+title: "Dealing with Difficult Personalities and Difficult Situations",
+},{
+title: "Building Personal Credibility: Understanding Different Communication Styles",
+},
+{
+title: "Motivating People for Change",
+},{
+title: Running Effective Meetings",
+},{
+title: "Building Confidence",
+},
+{
+title: "Team Building and Partnerships Across Departments",
+},{
+title: Communication Skills for Mediators",
+},{
+title: "Crisis Management",
+},{
+title: "Presentation Skills",
+},{
+title: Crisis Communication: Managing Complex Issues/Critical Situations",
+},{
+title: "Agile Working For Digital Transformation",
+},{
+title: Press Releases, Briefs and Press Conferences",
+},{
+title: "B2B Marketing- The Future",
+},{
+title: "Basic Principles of Good Procurement",
+},{
+title: Impact Of Digitisation On The Competitive Landscape",
+},{
+title: "Managing Across Cultures",
+
+},{
+title: "Lean Start Up",
+},{
+title: "Logistics Management",
+},{
+title: "Understanding Gender Perspectives In Business",
+}
+
+]
