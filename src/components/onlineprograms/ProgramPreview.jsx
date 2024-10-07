3c726628d665b0aebf77c5c7e8a6b88740b52126
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-07 04:51:51 -1100

    Update ProgramPreview.jsx

--- src/components/onlineprograms/ProgramPreview.jsx
+++ src/components/onlineprograms/ProgramPreview.jsx
@@ -7,13 +7,24 @@ return(
 {/* Left side*/}
 <div className='flex-1 w-full md:w-3/4'>
 
-<p className='text-xs md:text-sm text-gray-700 mb-3'>This course explores the dynamic landscape of digital marketing and its pivotal role in modern business strategies.</p>
-<p className='text-xs md:text-sm text-gray-700 my-2'>In today’s digital world, it is imperative for businesses to connect with a global audience, target specific demographics, measure campaign performance with precision, and deliver personalized marketing messages.</p>
+<p className='text-xs md:text-sm text-gray-700 mb-3 font-medium'>This course explores the dynamic landscape of digital marketing and its pivotal role in modern business strategies.</p>
+<p className='text-xs md:text-sm text-gray-700 my-2 font-medium'>In today’s digital world, it is imperative for businesses to connect with a global audience, target specific demographics, measure campaign performance with precision, and deliver personalized marketing messages.</p>
 
 <p className='text-sm font-medium text-black py-3'> Target Participants
 </p>
 <p className='text-sm text-red-600 font-medium'>For those in Digital Marketing
 </p>
+<p className='text-xl font-medium text-black py-3'> Course Objectives
+</p>
+<p className='text-[18px] font-medium text-black py-3'> At the end of the course, participants should be able to:
+</p>
+<ul className='gap-y-2 px-2'>
+<li className='text-sm text-gray-700 font-medium'>Define fundamental digital marketing concepts such as customerpersona, segmentation, funnels, and value propositions</li>
+<li className='text-sm text-gray-700 font-medium'>Define fundamental digital marketing concepts such as customerpersona, segmentation, funnels, and value propositions</li>
+<li className='text-sm text-gray-700 font-medium'>Define fundamental digital marketing concepts such as customerpersona, segmentation, funnels, and value propositions</li>
+</ul>
+
+
 </div>
 {/* Right side*/}
 <div className='w-full md:w-1/3 flex flex-col items-center' >
