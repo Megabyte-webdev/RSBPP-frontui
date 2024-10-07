fea4bc756e318e72737c899f3f0dc9a832510142
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-06 23:24:07 -1100

    Update programList.js

--- src/components/onlineprograms/programList.js
+++ src/components/onlineprograms/programList.js
@@ -11,30 +11,30 @@ title: "Building Personal Credibility: Understanding Different Communication Sty
 {
 title: "Motivating People for Change"
 },{
-title: Running Effective Meetings"
+title: "Running Effective Meetings"
 },{
 title: "Building Confidence"
 },
 {
 title: "Team Building and Partnerships Across Departments"
 },{
-title: Communication Skills for Mediators"
+title: "Communication Skills for Mediators"
 },{
 title: "Crisis Management"
 },{
 title: "Presentation Skills"
 },{
-title: Crisis Communication: Managing Complex Issues/Critical Situations"
+title: "Crisis Communication: Managing Complex Issues/Critical Situations"
 },{
 title: "Agile Working For Digital Transformation"
 },{
-title: Press Releases, Briefs and Press Conferences"
+title: "Press Releases, Briefs and Press Conferences"
 },{
 title: "B2B Marketing- The Future"
 },{
 title: "Basic Principles of Good Procurement"
 },{
-title: Impact Of Digitisation On The Competitive Landscape"
+title: "Impact Of Digitisation On The Competitive Landscape"
 },{
 title: "Managing Across Cultures"
 
