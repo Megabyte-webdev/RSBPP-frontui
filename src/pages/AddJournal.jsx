be6f515e89c08af0d2536240434fe1f2d1843852
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-15 06:58:10 -1100

    Update AddJournal.jsx

--- src/pages/AddJournal.jsx
+++ src/pages/AddJournal.jsx
@@ -15,7 +15,7 @@ return (
 <p className='sticky top-18 bg-transparent ml-auto my-2 flex items-center gap-2 font-medium'><BsJournalCheck size="24" />Add Journal</p>
 <div>
 {/* Dropdown */}
-<div className='font-medium my-2'>
+<div className='font-medium my-3'>
 <p className='text-sm md:text-xl my-2'>Choose RSBPP Faculty</p>
 <section className='flex justify-between items-center gap-2 border-[1px] border-red-500 rounded-md p-2 md:p-3'>
 <div className='flex flex-col gap-y-2'>
@@ -33,7 +33,7 @@ onChange={(e) => setFaculty(e.target.value)}
 <p className='border-l border-gray-500 pl-4 text-red-500'><IoIosArrowDown size='20' /></p>
 </section>
 </div>
-  <div className='font-medium my-2'>
+  <div className='font-medium my-3'>
       <section className='flex justify-between items-center gap-2 border-[1px] border-red-500 rounded-md p-2 md:p-3'>
         <div className='flex flex-col gap-y-2'>
           <p className='text-xs md:text-[16px] capitalize'>{course}</p>
@@ -52,7 +52,7 @@ onChange={(e) => setFaculty(e.target.value)}
       </section>
     </div>
 
-    <div className='font-medium my-2'>
+    <div className='font-medium my-3'>
       <section className='flex justify-between items-center gap-2 border-[1px] border-red-500 rounded-md p-2 md:p-3'>
         <div className='flex-1 flex flex-col gap-y-2'>
           <p className='text-sm md:text-[16px] capitalize'>Remark</p>
@@ -68,7 +68,7 @@ onChange={(e) => setFaculty(e.target.value)}
     </div>
 
   </div>
-<button className="my-2 mx-auto w-48 px-8 py-2 text-white bg-[navy] rounded-md font-medium">Submit</button>
+<button className="my-3 mx-auto w-48 px-8 py-2 text-white bg-[navy] rounded-md font-medium">Submit</button>
 </div>
 
 );
