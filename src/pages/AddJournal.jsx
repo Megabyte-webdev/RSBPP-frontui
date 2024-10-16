50ce156648f2839ded127e1326a6a9337d902f23
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-15 13:41:33 -1100

    Update AddJournal.jsx

--- src/pages/AddJournal.jsx
+++ src/pages/AddJournal.jsx
@@ -69,10 +69,8 @@ const AddJournal = () => {
               value={course}
               onChange={(e) => setCourse(e.target.value)}
             >
-{filteredData 
-?<option disabled selected value=""  className='rounded-md' >Select a Course From {filteredData?.title}</option>
-:<option disabled selected value="" className='rounded-md' >Select a Course</option>
-}
+<option disabled selected className='rounded-md' >Select a Course</option>
+
               {
                 filteredData && filteredData?.courses?.map((item, index) => (
                   <option className='rounded-md' key={index} value={item?.title}>{item?.title}</option>
