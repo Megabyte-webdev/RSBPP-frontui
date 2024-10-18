bcf057c5e05e723085661f696b0769396abd0bd9
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-18 11:59:37 -1100

    Update ProgramPreview.jsx

--- src/components/onlineprograms/ProgramPreview.jsx
+++ src/components/onlineprograms/ProgramPreview.jsx
@@ -6,7 +6,7 @@ import { useContext, useState } from 'react'; // Import useState for loading sta
 
 import toast from 'react-hot-toast'
 import './program.css';
-import BASE_URL from '../utils/base'
+import BASE_URL from '../../utils/base'
 const ProgramPreview = ({ details }) => {
     const navigate = useNavigate();
     const { userCredentials } = useContext(UserContext); // Get user credentials from context
