bc8392bb0c03f1b687b9bc98753d86ca15c58189
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-23 01:13:20 -1100

    Update ViewJournals.jsx

--- src/pages/ViewJournals.jsx
+++ src/pages/ViewJournals.jsx
@@ -97,7 +97,7 @@ const ViewJournals = () => {
                     disabled={currentPage === 1}
                     className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md"
                 >
-                    Previous
+                    Prev
                 </button>
                 <span>
                     Page {currentPage} of {totalPages}
