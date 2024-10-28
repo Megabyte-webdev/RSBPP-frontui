c8a207d94afd35f793fe947ee9ffc4ec91750e38
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-28 05:16:57 -1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -111,10 +111,10 @@ const App = () => {
               <Route path="/view-journals" element={<ViewJournals />} />
               <Route path="/upload-assignment" element={<UploadAssignment />} />
               <Route
-                path="/view-assignments/:course"
+                path="/submitted-assignments/:course"
                 element={<SubmittedAssignments />}
               />
-              <Route path="/view-assignments" element={<AllAssignment />} />
+              <Route path="/submitted-assignments" element={<AllAssignment />} />
               <Route
                 path="/view-grade/:assignment"
                 element={<ViewGrade />}
