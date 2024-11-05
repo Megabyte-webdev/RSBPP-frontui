eea1dc7d214b1c0323fdf0e9a62e3ce4b71a75cc
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-05 17:27:02 +0100

    Update Registration.jsx

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,10 @@
+import React, { useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import RegistrationGuide from '../components/auth/RegistrationGuide'
 import RegistrationForm from '../components/auth/RegistrationForm'
 import RegOtpForm from '../components/auth/RegOtpForm'
+import RegOnboarding from '../components/auth/RegOnboarding'
+import Widget from '../components/auth/Widget'
 
 const Registration = () => {
 
@@ -17,6 +20,7 @@ const Registration = () => {
                 <div className="d-flex h-100 align-items-center justify-content-center">
                     {display === "registration" && (<RegistrationForm setDisplay={setDisplay} />)}
                     {display === "otp" && (<RegOtpForm setDisplay={setDisplay} />)}
+                    {display === "onboarding" && (<Widget setDisplay={setDisplay} />)}
                     {/* {display === "onboarding" && (<RegOnboarding setDisplay={setDisplay} />)} */}
                     
                     {/* <RegOtpForm/> */}
